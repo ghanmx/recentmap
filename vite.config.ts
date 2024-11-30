639f10a4806f47a95e82f3b28cf7b0498f5ddcee
ghanmx <israelreyes16.ir@gmail.com>
2024-11-30 04:38:11 -0600

    @ghanmx

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { componentTagger } from 'lovable-tagger'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   server: {
     host: '::',
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
   },
   plugins: [react(), componentTagger()],
   resolve: {
